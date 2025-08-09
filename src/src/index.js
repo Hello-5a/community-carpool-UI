b7ac54b5d33af86078a93c5a7dd2175fb38b12b7
Hello-5a <reyanshgupta578@gmail.com>
2025-08-09 16:03:11 +0530

    Create index.js

---
+++ src/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CarpoolWebsite from './CarpoolWebsite';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<CarpoolWebsite />);
